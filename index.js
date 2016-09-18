7a8019687bdac1965b0ac4753b7dc7b6bb7eafee
Eric Zolayvar <ZolayvarECU@gmail.com>
2016-09-18 17:23:30 -0400

    Fix bug where response wasn't being parsed

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ request({
       'https://www.googleapis.com/youtube/v3/search',
       '?',
       '&part=id',
-      '&maxResults=50',
-      '&q=',
+      '&maxResults=5',
+      '&q=' + term,
+      '&type=video',
       '&key=AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g'
     ].join(''),
     method: 'GET',
   }, function (error, response, body) {
+    body = JSON.parse(body);
     var videoIds = [];
-    
+    body['items'].forEach(function (value, index) {
+      videoIds.push(value['id']['videoId'])
+    })
+
+    callback(videoIds);
   });
 }
-
 
 
 
