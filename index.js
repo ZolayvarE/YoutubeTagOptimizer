92afc2116e99cb9cc2da3f9e6dc5694d5eac9aa9
Eric Zolayvar <ZolayvarECU@gmail.com>
2016-09-18 16:33:54 -0400

    Initialize request module with options

---
+++ index.js
@@ -0,0 +1,40 @@
+var request = require('request');
+var keys = require('./helpers/keys.js');
+  
+request({
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  method: 'GET',
+  options: {
+
+  },
+}, function (error, response, body) {
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
