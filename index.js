68d1ca859c809dc63d18aa918a138efd7a01644a
Eric Zolayvar <ZolayvarECU@gmail.com>
2016-09-18 17:15:15 -0400

    Write function to get top video ids given a search term

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var request = require('request');
 var credentials = require('./helpers/keys.js');
-  
+
+var getTopVideoIds = function (term, callback) {
 request({
   url: [
-  	'https://www.googleapis.com/youtube/v3/search',
-  	'?',
-  	'&part=id',
-  	'&q=dogs',
-  	'&key=AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g'
-  ].join(''),
-  method: 'GET',
-}, function (error, response, body) {
-  console.log(body);
-});
+      'https://www.googleapis.com/youtube/v3/search',
+      '?',
+      '&part=id',
+      '&maxResults=50',
+      '&q=',
+      '&key=AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g'
+    ].join(''),
+    method: 'GET',
+  }, function (error, response, body) {
+    var videoIds = [];
+    
+  });
+}
 
 
 
