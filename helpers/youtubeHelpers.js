14d0d1a634b6f83c5ef157e7c8410d0c78696a65
Eric Zolayvar <ZolayvarECU@gmail.com>
2016-09-18 19:41:10 -0400

    Modularize code by using helper files

---
+++ helpers/youtubeHelpers.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var credentials = require('./keys.js');
+
+var getTopVideoIds = function (term, callback) {
+  request({
+    url: [
+        'https://www.googleapis.com/youtube/v3/search',
+        '?',
+        '&part=id',
+        '&maxResults=5',
+        '&q=' + term,
+        '&type=video',
+        '&key=AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g'
+    ].join(''),
+    method: 'GET',
+  }, function (error, response, body) {
+    body = JSON.parse(body);
+    var videoIds = [];
+    body['items'].forEach(function (value, index) {
+      videoIds.push(value['id']['videoId'])
+    })
+
+    callback(error, videoIds);
+  });
+}
+
+module.exports = {
+  getIdsFor: getTopVideoIds,
+};
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-var request = require('request');
-var credentials = require('./helpers/keys.js');
+var youtube = require('./helpers/youtubeHelpers.js');
 
-var getTopVideoIds = function (term, callback) {
-request({
-  url: [
-      'https://www.googleapis.com/youtube/v3/search',
-      '?',
-      '&part=id',
-      '&maxResults=5',
-      '&q=' + term,
-      '&type=video',
-      '&key=AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g'
-    ].join(''),
-    method: 'GET',
-  }, function (error, response, body) {
-    body = JSON.parse(body);
-    var videoIds = [];
-    body['items'].forEach(function (value, index) {
-      videoIds.push(value['id']['videoId'])
-    })
-
-    callback(videoIds);
-  });
-}
+youtube.getIdsFor('dogs', function (error, ids) {
+  console.log(ids);
+});
 
 
 
