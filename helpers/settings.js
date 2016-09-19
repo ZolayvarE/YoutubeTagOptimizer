e6e806b3be7476da842333081deb49d1d753825c
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-18 23:56:07 -0700

    Install middleware for oauth2

--- helpers/settings.js
+++ helpers/settings.js
@@ -1,4 +1,6 @@
 module.exports = {
   youtubeKey: 'AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g',
   defaultTags: ['Supply Line Studios', 'Pressure Gauge Gaming'],
+  clientID: '552208115327-19791jooknk80n0agadd18inajvcdv0t.apps.googleusercontent.com',
+  clientSecret: 'sdUGaycq4bxYQ8ynoRlmaJmn',
 };
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
+var settings = require('./helpers/settings.js');
 var express = require('express');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var passport = require('passport');
 var youtube = require('./helpers/youtubeHelpers.js');
+var strategy = require('passport-youtube-v3').Strategy;
+
+passport.use(new strategy({
+  clientID: settings.clientID,
+  clientSecret: settings.clientSecret,
+  callbackURL: 'http://localhost:3000/auth/youtube/callback',
+  scope: ['https://www.googleapis.com/auth/youtube']
+}, function(accessToken, refreshToken, profile, done) {
+  User.findOrCreate({ userId: profile.id }, function (err, user) {
+    return done(err, user);
+  });
+}));
+
+
 
 var app = express();
 
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/client'));
 
+app.get('/login', passport.authenticate(), function (req, res) {
+  console.log(req);
+  console.log(res);
+});
 
+app.get('/authenticated', function (req, res) {
+  console.log(req.body);
+  res.send('You did it!');
+});
 
-
-app.listen(process.env.PORT || 3571);
-
+var port = process.env.PORT || 3571;
+app.listen(port);
+console.log('Listening on port: ' + port);
 
 
 
