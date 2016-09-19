0414b474b5123e205a12bf8f77fd7a4367c2b52e
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-19 01:38:53 -0700

    Begin implementing oauth

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var youtube = require('./helpers/youtubeHelpers.js');
 var strategy = require('passport-youtube-v3').Strategy;
+var request = require('request');
+var n = require('nonce')();
 
-passport.use(new strategy({
-  clientID: settings.clientID,
-  clientSecret: settings.clientSecret,
-  callbackURL: 'http://localhost:3000/auth/youtube/callback',
-  scope: ['https://www.googleapis.com/auth/youtube']
-}, function(accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ userId: profile.id }, function (err, user) {
-    return done(err, user);
-  });
-}));
+var url = [
+  'https://accounts.google.com/o/oauth2/v2/auth',
+  '?',
+  '&scope=https://www.googleapis.com/auth/youtube',
+  '&response_type=code',
+  '&client_id=' + settings.clientID,
+  '&redirect_uri=http://localhost:3571/authenticated'
+].join('');
 
+// console.log('\n\nAuthentication Url:\n' + url + '\n\n')
 
 
 var app = express();
@@ -24,14 +25,42 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/client'));
 
+app.get('/', function (req, res) {
+  res.redirect(url);
+});
+
 app.get('/login', passport.authenticate(), function (req, res) {
   console.log(req);
   console.log(res);
 });
 
 app.get('/authenticated', function (req, res) {
-  console.log(req.body);
-  res.send('You did it!');
+  if (req.query.code) {
+    var tokenUrl = [
+      'https://accounts.google.com/o/oauth2/token',
+      '?',
+      '&grant_type=authorization_code',
+      '&code=' + req.query.code,
+      '&client_id=' + settings.clientID,
+      '&client_secret=' + settings.clientSecret,
+      '&redirect_uri=http://localhost:3571/authenticated',
+    ].join('');
+
+    console.log('\n\nToken Url:\n' + tokenUrl);
+
+    request.post(tokenUrl, {
+      multipart: [{
+        'content-type': 'application/x-www-form-urlencoded',
+        body: {},
+      }]
+      
+    }, function (error, response, body) {
+      console.log('\n\nError:\n' + error);
+      console.log('\n\nBody:\n' + body);
+    });
+
+    res.send('You did it!');
+  }
 });
 
 var port = process.env.PORT || 3571;
