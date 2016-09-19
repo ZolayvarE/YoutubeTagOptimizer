574dac04fd4b528c7c125fcf1083d54b47489336
Eric Zolayvar <ZolayvarECU@gmail.com>
2016-09-18 22:58:43 -0400

    Write function to get tags for topic.

--- helpers/youtubeHelpers.js
+++ helpers/youtubeHelpers.js
@@ -1,12 +1,16 @@
 var request = require('request');
 var credentials = require('./keys.js');
 
-var getTopVideoIds = function (term, callback) {
+var getTopVideoIds = function (number, term, callback) {
+  if (number === undefined) {
+    number = 25;
+  }
+
   request({
     url: [
       'https://www.googleapis.com/youtube/v3/search',
       '?part=id',
-      '&maxResults=5',
+      '&maxResults=' + Math.min(number, 50),
       '&q=' + term,
       '&type=video',
       '&key=AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g'
@@ -23,35 +27,73 @@ var getTopVideoIds = function (term, callback) {
   });
 }
 
-var getTagsForVideoById = function (videoIds, callback) {
-  if (!Array.prototype.isArray(videoId)) {
+var getVideoDetailsById = function (videoIds, callback) {
+  if (!Array.isArray(videoIds)) {
     videoIds = [videoIds];
   }
 
-  if (typeof videoIds[0] !== string) {
+  if (typeof videoIds[0] !== 'string') {
     callback('Did not get an array of video id strings', null);
   }
 
   request({
     url: [
       'https://www.googleapis.com/youtube/v3/videos',
-      '?key=' + credentials.youtubeKey,
+      '?part=snippet',
+      '&key=' + credentials.youtubeKey,
       '&id=' + videoIds.join(','),
-      
-
     ].join(''),
-    method: 'GET'
+    method: 'GET',
   }, function (error, response, body) {
-
-
-
-
-    callback(error);
+    callback(error, body);
   })
 }
 
+var getTagsForTopic = function (topic, callback, number) {
+  getTopVideoIds(number, topic, function (error, ids) {
+    if (error) {
+      callback(error, null);
+    } else {
+      getVideoDetailsById(ids, function (error, videos) {
+        if (error) {
+          callback(error, null);
+        } else { 
+          videos = JSON.parse(videos);
+          var results = [];
+          videos['items'].forEach(function (item) {
+            results = results.concat(item.snippet.tags);
+          })
+          
+          callback(null, results);
+        }
+      })
+    }
+  })
+}
+
+var sortTagsByPopularity = function (tags, callback) {
+  var countObject = {};
+  tags.forEach(function (tag) {
+    if (tag) {
+      tag = tag.toLowerCase();
+      countObject['' + tag] = countObject['' + tag] + 1 || 1;
+    }
+  });
+
+  var count, wordCount;
+  for (var key in countObject) {
+    count = countObject[key];
+    wordCount = key.split(' ').length;
+    countObject[key] = count + Math.min(count, wordCount) - 1;
+  }
+  
+  
+};
+
 module.exports = {
   getIdsFor: getTopVideoIds,
+  getVideoDetailsFor: getVideoDetailsById,
+  getTagsFor: getTagsForTopic,
 };
 
 
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var youtube = require('./helpers/youtubeHelpers.js');
 
-youtube.getIdsFor('dogs', function (error, ids) {
-  console.log(ids.join(','));
+youtube.getTagsFor('barbie', function (error, tags) {
+  if (error) {
+  	console.log(error);
+  } else {
+  	var countObject = {};
+   	tags.forEach(function (tag) {
+   	  if (tag) {
+  	    tag = tag.toLowerCase();
+  	    countObject[tag] = countObject[tag] + 1 || 1;
+   	  }
+  	});
+
+  	countObject;
+  }
 });
 
 
