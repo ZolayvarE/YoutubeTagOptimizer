05468dc77a7ab38c4aed61a622561f4c1eb4eb30
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-20 13:09:59 -0700

    Change layout of folders to allow for creation of client, side.

--- helpers/settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  youtubeKey: 'AIzaSyAjj_mH2B04zVPvHa54hEfTs9gwFw-0F6g',
-  defaultTags: ['Supply Line Studios', 'Pressure Gauge Gaming'],
-  clientID: '552208115327-19791jooknk80n0agadd18inajvcdv0t.apps.googleusercontent.com',
-  clientSecret: 'sdUGaycq4bxYQ8ynoRlmaJmn',
-  channelId: 'UC_keYIDrj5wwwAZv2OgPHYA',
-};
